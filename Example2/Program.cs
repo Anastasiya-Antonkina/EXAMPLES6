6b46038884fdc7cec598670e0b7b7b9d5d4dec1b
Antonkina Anastaiya <floweri888@mail.ru>
2022-12-13 08:48:17 +0300

    Add examples

---
+++ Example2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Введите число");
+string number = (Console.ReadLine());
+
+if (!int.TryParse(number, out int val)) return;
+
+int sum = 0;
+for (int i = 0; i < number.Length; i++)
+    sum += int.Parse(number[i].ToString());
+
+Console.Write($"Сумма цифр в числе {number} равна: {sum}");
